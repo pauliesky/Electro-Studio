e2ee9f6344f307580170b91e5d95f8dab4e4bf94
Akinleye Paul <paulakinleye@gmail.com>
2023-01-04 18:35:22 +0100

    implemented home page

--- src/Components/Routes/Home.js
+++ src/Components/Routes/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import videoBG from '../Images/course-video.mp4'
+import '../Styles/Home.css'
+
+
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="overlay"></div>
+      <video src={videoBG} autoPlay loop muted />
+      <div className='content'>
+        <h1>WELCOME</h1>
+      </div>
+    </div>
   )
 }
 
